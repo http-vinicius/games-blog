b8aa512bfa7cd94c991c873100396433f544b9bc
Vinicius Santos <vinicius.santos@sysalti.com>
2026-01-21 17:58:09 -0300

    Configurações iniciais do tema e font

---
+++ eslint.config.js
@@ -0,0 +1,46 @@
+import pluginJs from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import pluginImport from 'eslint-plugin-import';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  prettier,
+  {
+    plugins: {
+      'react-hooks': pluginReactHooks,
+      import: pluginImport,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      // 'import/order': [
+      //   'warn',
+      //   {
+      //     groups: [
+      //       'builtin',
+      //       'external',
+      //       'internal',
+      //       'parent',
+      //       'sibling',
+      //       'index',
+      //     ],
+      //     'newlines-between': 'always',
+      //     alphabetize: { order: 'asc', caseInsensitive: true },
+      //   },
+      // ],
+      // 'import/first': 'warn',
+      // 'import/newline-after-import': 'warn',
+      // 'import/no-duplicates': 'warn',
+    },
+  },
+];
